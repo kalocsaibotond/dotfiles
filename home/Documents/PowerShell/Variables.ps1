bf487b5df6406a4dc5ae36ca7d80c40422db880a
Botond Kalocsai <90713156+kalocsaibotond@users.noreply.github.com>
2024-03-20 22:49:56 +0100

    Update Variables.ps1

--- home/Documents/PowerShell/Variables.ps1
+++ home/Documents/PowerShell/Variables.ps1
@@ -1,7 +1,8 @@
 # XDG Base directories
 $XDG_DATA_HOME="$ENV:HOME\.local\share"
 $XDG_CONFIG_HOME="$ENV:HOME\.config"
-$XDG_STATE_HOME="$ENV:HOME\.local/state "
+$XDG_STATE_HOME="$ENV:HOME\.local\state"
+$XDG_CACHE_HOME="$ENV:HOME\.cache"
 
 # Accessories
 $EDITOR=nvim
