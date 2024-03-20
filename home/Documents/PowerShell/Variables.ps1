027ecccaaf549a3614d73bdd535a973121320ff2
Botond Kalocsai <90713156+kalocsaibotond@users.noreply.github.com>
2024-03-20 22:17:22 +0100

    Create Variables.ps1

---
+++ home/Documents/PowerShell/Variables.ps1
@@ -0,0 +1,8 @@
+# XDG Base directories
+$XDG_DATA_HOME="$ENV:HOME\.local\share"
+$XDG_CONFIG_HOME="$ENV:HOME\.config"
+$XDG_STATE_HOME="$ENV:HOME\.local/state "
+
+# Accessories
+$EDITOR=nvim
+$VISUAL=$EDITOR
