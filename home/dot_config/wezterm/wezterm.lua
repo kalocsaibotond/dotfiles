5ec3b6c2761808929c0f501755fcc51f05fc3692
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2024-03-22 12:14:14 +0100

    fix(wezterm): fixing default prog config by eliminating execution policy

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -6,10 +6,9 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- TODO: Check os and shell availability and define logic for preference
 config.default_prog = {
-	[[
-    pwsh -ExecutionPolicy Bypass
-  ]],
+	[[pwsh]],
 }
 
 return config
