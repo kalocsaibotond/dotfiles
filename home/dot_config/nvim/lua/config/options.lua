c8a8d74362683ec062264754436eb88b3bb4b7f2
Botond Kalocsai <kalocsaibotond@gmail.com>
2024-03-30 21:07:03 +0100

    feat(wezterm): introducing proper shell config logic and trying out OpenDyslexic nerdfont

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -11,29 +11,33 @@ vim.opt.colorcolumn = "79"
 
 -- Setting default shell
 if "cmd.exe" == vim.o.shell and vim.fn.executable("powershell") then
-  if vim.fn.executable("pwsh") then
-    vim.o.shell = "pwsh"
-  else
-    vim.o.shell = "powershell"
-  end
+	if vim.fn.executable("pwsh") then
+		vim.o.shell = "pwsh"
+	else
+		vim.o.shell = "powershell"
+	end
 
-  vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy Bypass -Command "
-  --   .. "[Console]::InputEncoding=[Console]"
-  --   .. "::OutputEncoding=[System.Text.UTF8Encoding]::new();"
-  --   .. "$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
-  --   .. "Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
-  -- vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-  -- vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
-  vim.o.shellredir = "| Out-File -Encoding UTF8 %s"
-  vim.o.shellpipe = "| Out-File -Encoding UTF8 %s"
-  vim.o.shellquote = ""
-  vim.o.shellxquote = ""
+	vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy Bypass -Command "
+	--   .. "[Console]::InputEncoding=[Console]"
+	--   .. "::OutputEncoding=[System.Text.UTF8Encoding]::new();"
+	--   .. "$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+	--   .. "Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+	-- vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+	-- vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+	vim.o.shellredir = "| Out-File -Encoding UTF8 %s"
+	vim.o.shellpipe = "| Out-File -Encoding UTF8 %s"
+	vim.o.shellquote = ""
+	vim.o.shellxquote = ""
 end
 
 -- Setting up python3 provider
 local conda_prefix = os.getenv("CONDA_PREFIX")
 if nil == conda_prefix then
-  vim.g.python3_host_prog = vim.env.HOME .. psep .. "Anaconda3" .. psep .. "python"
+	vim.g.python3_host_prog = vim.env.HOME
+		.. psep
+		.. "Anaconda3"
+		.. psep
+		.. "python"
 else
-  vim.g.python3_host_prog = conda_prefix .. psep .. "python"
+	vim.g.python3_host_prog = conda_prefix .. psep .. "python"
 end
--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,14 +1,45 @@
-local wezterm = require("wezterm")
+local wezterm = require("wezterm") -- wezterm api
 
+-- Initialising configuration table
 local config = {}
 
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- TODO: Check os and shell availability and define logic for preference
-config.default_prog = {
-	[[pwsh]],
-}
+-- Setting shell config logic
+if wezterm.target_triple:find("windows") then
+	local success, stdout, _ = wezterm.run_child_process({
+		"powershell",
+		"-Command",
+		"[bool](Get-Command -Name pwsh -ErrorAction SilentlyContinue)",
+	})
+	if success then
+		if stdout:find("True") then
+			config.default_prog = {
+				"pwsh",
+				"-NoExit",
+				"-ExecutionPolicy",
+				"Bypass",
+			}
+		else
+			config.default_prog = {
+				"powershell",
+				"-NoExit",
+				"-ExecutionPolicy",
+				"Bypass",
+			}
+		end
+	end
+end
+
+-- Things to do on gui startup
+wezterm.on("gui-startup", function()
+	local tab, pane, window = wezterm.mux.spawn_window({})
+	window:gui_window():maximize() -- Maximize window
+	window:gui_window():toggle_fullscreen() -- Toggle to Full screen
+end)
+
+config.font = wezterm.font("OpenDyslexicM Nerd Font Mono")
 
 return config
