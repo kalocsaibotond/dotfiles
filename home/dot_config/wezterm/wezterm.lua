d9475c4639212f0788999479e206292a458cfade
Botond Kalocsai <kalocsaibotond@gmail.com>
2024-04-02 11:18:12 +0200

    feat(wezterm): hidihg tab bar when one tab is present and setting new key for fullscreen
    The Workspacer's ALT - Enter key interferes.

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -33,15 +33,24 @@ if wezterm.target_triple:find("windows") then
 	end
 end
 
+-- Display settings
 config.window_decorations = "RESIZE"
--- Things to do on gui startup
+config.hide_tab_bar_if_only_one_tab = true
 wezterm.on("gui-startup", function()
 	local tab, pane, window = wezterm.mux.spawn_window({})
 	window:gui_window():maximize() -- Maximize window
 	window:gui_window():toggle_fullscreen() -- Toggle to Full screen
 end)
-
 config.font = wezterm.font("OpenDyslexicM Nerd Font Mono")
 config.font_size = 10
+
+-- Custom keybindings
+config.keys = {
+	{ -- ALT - Enter is used by workspacer
+		key = "Enter",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
 
 return config
