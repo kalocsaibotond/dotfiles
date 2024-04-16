a27b5877e68fbf645bb81baa13490dcb850582f9
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2024-04-16 20:00:11 +0200

    feat(vscodium): adding simple vscodium config for jupyter notebook editing
    It is worth to have a simple vscodium config, since it is open source
    and Neovim can be fully intagrated into it. I intend to use it for
    jupyter notebook editing, until I properly solve editing jupyter
    notebooks in Neovim or Terminal.

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -2,18 +2,22 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- iron also has a list of commands, see :h iron-commands for all available commands
-vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
-vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
-vim.keymap.set("n", "<space>rF", "<cmd>IronFocus<cr>")
-vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+if vim.g.vscode then
+-- vscode-nvim settings
+else
+	-- iron also has a list of commands, see :h iron-commands for all available commands
+	vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
+	vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
+	vim.keymap.set("n", "<space>rF", "<cmd>IronFocus<cr>")
+	vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
 
--- Chezmoi
-local telescope = require("telescope")
-telescope.load_extension("chezmoi")
-vim.keymap.set(
-	"n",
-	"<leader>fz",
-	telescope.extensions.chezmoi.find_files,
-	{ desc = "Find Chezmoi Source File" }
-)
+	-- Chezmoi
+	local telescope = require("telescope")
+	telescope.load_extension("chezmoi")
+	vim.keymap.set(
+		"n",
+		"<leader>fz",
+		telescope.extensions.chezmoi.find_files,
+		{ desc = "Find Chezmoi Source File" }
+	)
+end
