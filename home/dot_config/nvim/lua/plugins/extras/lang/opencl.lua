e198d7df49585b249c979237bf4e23638b092cbe
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2024-05-12 23:17:34 +0200

    feat(LazyVim): add autocommand for file defaults, lsp config, formatter for OpenCL user extra

--- home/dot_config/nvim/lua/plugins/extras/lang/opencl.lua
+++ home/dot_config/nvim/lua/plugins/extras/lang/opencl.lua
@@ -1,5 +1,36 @@
 return {
 	{
 		"brgmnn/vim-opencl",
+	}, -- NOTE: Contains OpenCL file detection autocommand
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			setup = {
+				opencl_ls = function()
+					require("lspconfig").opencl_ls.setup({})
+				end,
+			},
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			table.insert(opts.ensure_installed, "clang-format")
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				["opencl"] = { "clang-format" },
+			},
+			formatters = {
+				["clang-format"] = {
+					args = {
+						"--style={BasedOnStyle: chromium, IndentWidth: 8}",
+					},
+				},
+			},
+		},
 	},
 }
