108bbbfd2b3efbe5e7298bedb89c0f15e73940c4
Botond Kalocsai <90713156+kalocsaibotond@users.noreply.github.com>
2024-07-23 12:33:05 +0200

    feat(LazyVim): add aerial.nvim extra to main config

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.yanky" },
 		{ import = "lazyvim.plugins.extras.editor.dial" },
 		{ import = "lazyvim.plugins.extras.editor.harpoon2" },
+		{ import = "lazyvim.plugins.extras.editor.aerial" },
 		{ import = "plugins" },
 	},
 	defaults = {
