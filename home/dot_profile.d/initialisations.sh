2e8b16a54556ff447c08e20043bd6c5cdd9cb06b
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2024-12-01 22:40:13 +0100

    fix(posix): correct brew initialisaiton

--- home/dot_profile.d/initialisations.sh
+++ home/dot_profile.d/initialisations.sh
@@ -1,6 +1,6 @@
 if ! command -v brew; then
   brew_location="/home/linuxbrew/.linuxbrew/bin/brew"
-  if [ -f $brew_location ];
+  if [ -f $brew_location ]; then
     eval "$($brew_location shellenv)"
   fi
 fi
