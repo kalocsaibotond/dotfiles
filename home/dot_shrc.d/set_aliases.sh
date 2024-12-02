9fe552b0724c1c73b7ae974b6c1303f99a8c36e2
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2024-12-02 21:01:02 +0100

    fix(posix): make ezap, fdp, rgp aliases into function to be able to pass arguments into them

--- home/dot_shrc.d/set_aliases.sh
+++ home/dot_shrc.d/set_aliases.sh
@@ -1,6 +1,12 @@
 alias pvim='nvim -c $XDG_CONFIG_HOME/nvim-pluginless/init.vim'
 
 alias eza="eza --icons=auto"
-alias ezap="eza --colour=always --icons=always --classify=always | $PAGER"
-alias fdp="fd --color always | $PAGER"
-alias rgp="rg --pretty | $PAGER"
+ezap() {
+  eza --colour=always --icons=always --classify=always "$@" | $PAGER
+}
+fdp() {
+  fd --color always "$@" | $PAGER
+}
+rgp() {
+  rg --pretty "$@" | $PAGER
+}
