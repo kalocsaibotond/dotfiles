ae9e8f796fe842805f32b6be0816d65c8b49f1f2
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2025-02-20 13:27:53 +0100

    feat(LazyVim): add escape from terminal mode to normal mode <esc><esc> keybinding
    The latest LazyVim releases only set keybindings to Snacks terminal, in
    which <esc><esc> escapes from terminal to normal mode. For the sake of
    consistency, and the fact that the default keyword does not wrok on
    windows, I set it to <esc><esc>.

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- if vim.g.vscode then
--- -- vscode-nvim settings
--- else
---  -- Ordinary nvim settings
--- end
+if vim.g.vscode then
+-- vscode-nvim settings
+else
+	-- Ordinary nvim settings
+	local map = vim.keymap.set
+
+	map(
+		"t",
+		"<esc><esc>",
+		[[<C-\><C-n>]],
+		{ desc = "Escape from terminal mode to Normal mode" }
+	)
+end
