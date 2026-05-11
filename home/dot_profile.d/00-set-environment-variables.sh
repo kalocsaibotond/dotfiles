ef03fd1b5c3982dbdeefafef5e52bd0035444f41
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2026-05-11 19:14:01 +0200

    feat(profile): make adaptive envinment variable settint, rename pluginless vim config

--- home/dot_profile.d/00-set-environment-variables.sh
+++ home/dot_profile.d/00-set-environment-variables.sh
@@ -2,48 +2,88 @@
 if [ -d "$HOME/bin" ]; then
   case ":$PATH:" in
   *":$HOME/bin:"*) ;;
-  *) PATH="$HOME/bin:$PATH" ;;
+  *) export PATH="$HOME/bin:$PATH" ;;
   esac
 fi
 if [ -d "$HOME/.local/bin" ]; then
   case ":$PATH:" in
   *":$HOME/.local/bin:"*) ;;
-  *) PATH="$HOME/.local/bin:$PATH" ;;
+  *) export PATH="$HOME/.local/bin:$PATH" ;;
   esac
 fi
-export PATH
 
 export ENV="$HOME/.shrc"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
-export EDITOR="nvim"
-export VISUAL="nvim"
-export PAGER="moar"
-export MANPAGER="moar"
 
-# Configuring bat pager
-export BAT_PAGER="moar -no-linenumbers -quit-if-one-screen"
+if command -v 'nvim' >'/dev/null' 2>&1; then
+  if [ -r "$XDG_CONFIG_HOME/vim-pluginless/init.vim" ]; then
+    export EDITOR='nvim -u "$XDG_CONFIG_HOME/vim-pluginless/init.vim"'
+  else
+    export EDITOR='nvim --clean'
+  fi
+elif command -v 'vim' >'/dev/null' 2>&1; then
+  if [ -r "$XDG_CONFIG_HOME/vim-pluginless/init.vim" ]; then
+    export EDITOR='vim -u "$XDG_CONFIG_HOME/vim-pluginless/init.vim"'
+  else
+    export EDITOR='vim --clean'
+  fi
+elif command -v 'vi' >'/dev/null' 2>&1; then
+  export EDITOR='vi'
+elif command -v 'ex' >'/dev/null' 2>&1; then
+  export EDITOR='ex'
+elif command -v 'ed' >'/dev/null' 2>&1; then
+  export EDITOR='ed'
+fi
+
+if command -v 'nvim' >'/dev/null' 2>&1; then
+  export VISUAL='nvim'
+elif command -v 'vim' >'/dev/null' 2>&1; then
+  export VISUAL='vim'
+else
+  export VISUAL="$EDITOR"
+fi
+
+if command -v 'moor' >'/dev/null' 2>&1; then
+  export PAGER='moor'
+  export MANPAGER='moor'
+  export BAT_PAGER='moor -no-linenumbers -quit-if-one-screen'
+elif command -v 'moar' >'/dev/null' 2>&1; then
+  export PAGER='moar'
+  export MANPAGER='moar'
+  export BAT_PAGER='moar -no-linenumbers -quit-if-one-screen'
+elif command -v 'less' >'/dev/null' 2>&1; then
+  export PAGER='less -R'
+  export MANPAGER='less'
+else
+  export PAGER='more'
+  export MANPAGER='more'
+fi
+
 export BAT_CONFIG_DIR="$XDG_CONFIG_HOME/bat"
 
 # Configuring nnn file manager
 NNN_PLUG_OFFICIAL='z:autojump;f:finder;o:fzopen;p:preview-tabbed'
 if [ -h "$XDG_CONFIG_HOME/nnn/plugins/official" ] ||
   [ -d "$XDG_CONFIG_HOME/nnn/plugins/official" ]; then
-  NNN_PLUG=$(echo $NNN_PLUG_OFFICIAL | sed 's/:/:official\//g')
+  export NNN_PLUG="$(echo "$NNN_PLUG_OFFICIAL" | sed 's/:/:official\//g')"
 else
-  NNN_PLUG=$NNN_PLUG_OFFICIAL
+  export NNN_PLUG="$NNN_PLUG_OFFICIAL"
 fi
 unset NNN_PLUG_OFFICIAL
-export NNN_PLUG
 
 if [ -z "$NNN_OPENER" ]; then
-  export NNN_OPENER="$XDG_CONFIG_HOME/nnn/plugins/nuke"
+  if [ -x "$XDG_CONFIG_HOME/nnn/plugins/official/nuke" ]; then
+    NNN_OPENER="$XDG_CONFIG_HOME/nnn/plugins/official/nuke"
+  elif [ -x "$XDG_CONFIG_HOME/nnn/plugins/nuke" ]; then
+    export NNN_OPENER="$XDG_CONFIG_HOME/nnn/plugins/nuke"
+  fi
 fi
 if [ -z "$NNN_FIFO" ]; then
-  export NNN_FIFO="/tmp/nnn.fifo"
+  export NNN_FIFO='/tmp/nnn.fifo'
 fi
 if [ -z "$NNN_SEL" ]; then
-  export NNN_SEL=/tmp/.sel
+  export NNN_SEL='/tmp/.sel'
 fi
--- home/dot_shrc.d/99-set-aliases.sh
+++ home/dot_shrc.d/99-set-aliases.sh
@@ -1,54 +1,56 @@
-alias pvim='nvim -u $XDG_CONFIG_HOME/nvim-pluginless/init.vim'
+alias pvim='nvim -u "$XDG_CONFIG_HOME/vim-pluginless/init.vim"'
 
 # NOTE: These only work if the directory names do not contain \n.
 # Even though fzf has an --read0 flag, posix find to not support --print0 flag.
-if command -v eza >/dev/null 2>&1; then
+if command -v 'eza' >'/dev/null' 2>&1; then
   scd() {
     cd $(
-      find . -type d -print 2>/dev/null | fzf --preview \
+      find '.' -type 'd' -print 2>'/dev/null' | fzf --preview \
         'eza -G --colour=always --icons=always --classify=always {}' "$@"
     )
   }
   ucd() {
     cd $(
-      find $HOME -type d -print 2>/dev/null | fzf --preview \
+      find "$HOME" -type 'd' -print 2>'/dev/null' | fzf --preview \
         'eza -G --colour=always --icons=always --classify=always {}' "$@"
     )
   }
 else
   scd() {
-    cd $(find . -type d -print 2>/dev/null | fzf --preview 'ls -C {}' "$@")
+    cd $(find '.' -type 'd' -print 2>'/dev/null' |
+      fzf --preview 'ls -C {}' "$@")
   }
   ucd() {
-    cd $(find $HOME -type d -print 2>/dev/null | fzf --preview 'ls -C {}' "$@")
+    cd $(find "$HOME" -type 'd' -print 2>'/dev/null' |
+      fzf --preview 'ls -C {}' "$@")
   }
 fi
 
 alias eza="eza --icons=auto"
 ezap() {
-  eza -G --colour=always --icons=always --classify=always "$@" | $PAGER
+  eza -G --colour='always' --icons='always' --classify='always' "$@" | $PAGER
 }
 fdp() {
-  fd --color always "$@" | $PAGER
+  fd --color 'always' "$@" | $PAGER
 }
 rgp() {
   rg --pretty "$@" | $PAGER
 }
 difftp() {
-  difft --color always "$@" | $PAGER
+  difft --color 'always' "$@" | $PAGER
 }
 batf() {
   bat $(fzf --preview \
     'bat --style=numbers --color=always --line-range :100 {}' "$@")
 }
-if command -v bat >/dev/null 2>&1; then
+if command -v 'bat' >'/dev/null' 2>&1; then
   pagerf() {
-    "$PAGER" $(fzf --preview \
+    $PAGER $(fzf --preview \
       'bat --style=numbers --color=always --line-range :100 {}' "$@")
   }
 else
   pagerf() {
-    "$PAGER" $(fzf --preview 'head -n 100 {}' "$@")
+    $PAGER $(fzf --preview 'head -n 100 {}' "$@")
   }
 fi
 
