e3474550e19d3f180ba37502283a092ce4a2e58d
Botond Kalocsai <kalocsaibotond@gmail.com>
2026-07-08 15:32:31 +0200

    fix(LazyVim): setting g:gutentags_trace to 1 on windows
    This is a temporary measure to circunvent a bug where vim-gutentags just
    dumps its log at the cursor.

--- home/dot_config/nvim/lua/plugins/ctags.lua
+++ home/dot_config/nvim/lua/plugins/ctags.lua
@@ -1,4 +1,10 @@
 return {
 	"ludovicchabant/vim-gutentags",
 	event = "LazyFile",
+	init = function()
+		local uname = vim.uv.os_uname().sysname
+		if "Windows_NT" == uname then
+			vim.g.gutentags_trace = 1
+		end
+	end,
 }
