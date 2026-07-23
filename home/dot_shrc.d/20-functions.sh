2f8460a5077496552d879e9ea2185023a684bc67
Botond Kalocsai <kalocsaibotond@gmail.com>
2026-07-23 15:27:29 +0200

    feat(shrc): add ssh file copy to local system clipboard.

--- home/dot_shrc.d/20-functions.sh
+++ home/dot_shrc.d/20-functions.sh
@@ -16,3 +16,21 @@ nnn() {
   }
 
 }
+
+# This function copies the file content of an SSH remote system to the local
+# system's clipboard.
+# This function needs OSC 52 protocol support of the terminal emulator to work
+# emulator to work.
+# Through tmux this means the activation of clipboard allow-passthrough options.
+osc52copy() {
+  if [ -n "$1" ]; then
+    if [ -f "$1" ]; then
+      printf "\033]52;c;%s\033\\" "$(base64 -w 0 "$1")"
+    else
+      echo "Error: '$1' invalid file!" >&2
+      return 1
+    fi
+  else
+    printf "\033]52;c;%s\033\\" "$(base64 -w 0)"
+  fi
+}
